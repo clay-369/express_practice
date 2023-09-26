37032ff245abc6532bb58bf6a444ddbd553a5ff8
Clayton-Lente <clayton@lentedgital.nl>
2023-09-26 15:22:44 +0200

    Succesfully has Prisma

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;  // Change port to 3000 or another available port
 
-app.get('/', (req,res) =>{
-    res.send('Home page');
-})
+
 
 app.listen(port, () => {
     console.log('listening on port: ' + port)
