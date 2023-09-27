2f5412ead6e2e3fcb0b8c9397152ec5f910456a0
Clayton-Lente <clayton@lentedgital.nl>
2023-09-27 09:54:21 +0200

    get request and prisma schema fix

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
 const express = require('express');
 const app = express();
-const port = 3000;  // Change port to 3000 or another available port
+const port = 3000;
 
 // Prisma client configurations 
 const { PrismaClient } = require('@prisma/client');
+
 const prisma = new PrismaClient();
 
+// Defining data to add Users to the database
+async function createUsers() {
+  await Promise.all([
+      prisma.user.create({
+          data: {
+              name: 'John Doe',
+              email: 'john@example.com',
+              age: 30
+          }
+      }),
+      prisma.user.create({
+          data: {
+              name: 'Jane Smith',
+              email: 'jane@example.com',
+              age: 25
+          }
+      })
+      // Add more users if needed
+  ]);
+}
+// Toevoegen van gebruikers aan de database
+createUsers()
+  .catch((error) => console.error('Fout bij het toevoegen van gebruikers:', error))
+  .finally(() => prisma.$disconnect());
 
-// Retrieving all users
+// Ophalen van alle gebruikers
 app.get('/', async (req, res) => {
-    try {
-      const users = await prisma.user.findMany();
-      res.json(users);
-    } catch (error) {
-      console.error('Fout bij het ophalen van gebruikers:', error);
-      res.status(500).json({ error: 'Er is een serverfout opgetreden.' });
-    }
-  });
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    console.error('Fout bij het ophalen van gebruikers:', error);
+    res.status(500).json({ error: 'Er is een serverfout opgetreden.', details: error.message });
+  }
+});
 
 app.listen(port, () => {
-    console.log('listening on port: ' + port)
-})
+  console.log('Luisteren op poort: ' + port);
+});
