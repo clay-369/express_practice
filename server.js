167d3dcb3de6b725e303048c008e93eaa7fdecf1
Clayton-Lente <clayton@lentedgital.nl>
2023-09-27 09:59:00 +0200

    checks if the users already exist

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
+// Check if there are any users in the database
+async function checkUsers() {
+  const users = await prisma.user.findMany();
+  return users.length === 0;
+}
+
 // Defining data to add Users to the database
 async function createUsers() {
   await Promise.all([
@@ -27,9 +33,18 @@ async function createUsers() {
       // Add more users if needed
   ]);
 }
-// Toevoegen van gebruikers aan de database
-createUsers()
-  .catch((error) => console.error('Fout bij het toevoegen van gebruikers:', error))
+
+// Check if there are no users, then create users
+checkUsers()
+  .then(async (noUsers) => {
+    if (noUsers) {
+      await createUsers();
+      console.log('Users created successfully.');
+    } else {
+      console.log('Users already exist in the database.');
+    }
+  })
+  .catch((error) => console.error('Error checking users:', error))
   .finally(() => prisma.$disconnect());
 
 // Ophalen van alle gebruikers
